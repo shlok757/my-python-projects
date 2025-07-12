2d3ef9109ec61ecd7391c1fd600e9acce15fcf7a
shlok757 <singhpayalmath@gmail.com>
2025-07-12 20:58:36 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+
+a=[1,5,2,10]
+print(a)
+print(a[2])
+print(type(a))
+b=["schools bad because it we have to do exams and i do not get good marks so ban school from the world and everyone should not go to school because we have to wake up in the morning but if there is no school we can sleep till 12:00pm second reason is no body in the world likes school like bro nobody like school north korea should nuke all schools",10.3]
+print(b)
+print(type(b))
+m=("hi",54546)
+print(m)
+print(m[0])
+print(type(m))
+a[2]=50
+print(a)
+m(1)=7890
+print(m)
