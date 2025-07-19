530f27b48235ea35ebcdb95c10251cc936f4ee7d
shlok757 <singhpayalmath@gmail.com>
2025-07-19 20:59:35 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,28 @@
+m=[0]
+print("multiply 10 by 60")
+print("a.600 \nb.100000000000000000000 \nc.123\nd.123342")
+g=input("which is the correct option")
+
+if g=="a":
+   print("correct")
+   m=+1
+   print("your score is",m)
+   
+else:
+   print("bro your math is not mathing or your brain is not braining")
+   m=-1   
+   print("your score is",m)
+print("what is 60 plus 60")
+print("a.607 \nb.12o \nc.128\nd.123 ")
+w=input("which is the correct option")
+if w=="b":
+   print("correct")
+   m=+1
+   print("your score is",m)
+else:
+   print("bro is in playgroup")
+   m=-1
+   print("your score is",m)
+
+   
+ 
