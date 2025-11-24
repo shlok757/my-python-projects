653edc8a25237d2c925bdda40e166c1a360585fe
shlok757 <singhpayalmath@gmail.com>
2025-11-24 21:53:10 +0530

    Add files via upload

---
+++ wehave5differentvariables.py
@@ -0,0 +1,6 @@
+birthdayone = "2014/8/25"
+birthadaytwo = "2013/9/12"
+birthdaythree = "1985/4/4"
+birthdayfour = "1985/1/3"
+birthdayfive ="1957/3/6"
+print("my families 5 birthdays are ",birthdayone,birthadaytwo,birthdaythree,birthdayfour,birthdayfive)
