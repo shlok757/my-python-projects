a2cdc761f9f3dcdcb85b4a1dbd90b82e0b5e1c88
shlok757 <singhpayalmath@gmail.com>
2025-11-25 21:28:59 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+name = "Penguin"
+age = 15 
+is_student = True
+weight = 38.5
+print("Name",name)
+print("data type of name is",type(name))
+print("age",age)
+print("data type of age is",type(age) )
+print("is_student",is_student)
+print("data type of is_student is",type(is_student) )
+print("weight",weight)
+print("data type of weight is",type(weight) )
+
+
+
