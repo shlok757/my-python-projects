6157125d59af545c2ffb887af209b6ccf243ebca
shlok757 <singhpayalmath@gmail.com>
2025-12-08 21:33:02 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,12 @@
+height = input("enter ur height in cm")
+weight = input("enter ur weight in kg")
+BMI = weight / (height/100)**2
+print("your bmi is ",BMI)
+if BMI <= 18.4:
+    print("your underweight"): # type: ignore
+    elif BMI <= 24.9
+print("you are healthy")
+elif BMI <= 34.9
+print("your obese")
+else
+print("your overly obese")
