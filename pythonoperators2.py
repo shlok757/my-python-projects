91f77da54e467c9fea9d845c5fbfe2f5124d0dff
shlok757 <singhpayalmath@gmail.com>
2025-12-08 21:20:16 +0530

    Add files via upload

---
+++ pythonoperators2.py
@@ -0,0 +1,22 @@
+a = 10
+b = -10
+c = 0
+if a and b and c:
+    print("all number have boolean value")
+else:
+    print("atleast one number not has boolean value")
+    a = 10
+    b = -10
+    c = 0
+    if a > 0 b > 0
+print("either of the number id greater than 0")
+else
+print("no number is greater")
+if b > 0 c > 0
+print("either of the number is greater")
+else 
+print("no number is greater than 0")
+
+
+    
+    
