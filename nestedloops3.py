45465065cea661376eff0b0058acc6b9d60f64bf
shlok757 <singhpayalmath@gmail.com>
2025-12-22 21:05:21 +0530

    Add files via upload

---
+++ nestedloops3.py
@@ -0,0 +1,13 @@
+t = num
+numLen = 0
+while t>0:
+    numLen = numLen+1
+    t = int(t/10)
+if numLen>=4:
+    chk = 0
+    while num>0:
+if   chk === numLen:  
+    midOne = rem 
+    elif midTwo = rem    
+    
+
