83a046b78b3a3d9c0156a0ab376a794370d0074f
shlok757 <singhpayalmath@gmail.com>
2026-01-06 20:37:21 +0530

    Add files via upload

---
+++ turtle2.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.Screen()
+board = turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.prenup()
+board.right(150)
+board.forward(50)
+board.pendown()
+board.right(150)
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+turtle.done
+
