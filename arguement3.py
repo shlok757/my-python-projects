2fb8a1c47ed436d5fc0a6a756cd093aa37de989a
shlok757 <singhpayalmath@gmail.com>
2026-01-13 19:31:50 +0530

    Add files via upload

---
+++ arguement3.py
@@ -0,0 +1,11 @@
+def factorial(x):
+    if x==0 or x==1:
+    else:
+        return x*factorial(x-1)
+print(factorial._doc_)
+print("the factorial of 0;",factorial(0))
+print("the factorial of 1;",factorial(1))
+print("the factorial of 4;",factorial(4))
+print("the factorial of 5;",factorial(5))
+print("the factorial of 10;",factorial(10))
+
