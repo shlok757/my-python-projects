4a20d2829afe0784312337f68223ad144bb19d0c
shlok757 <singhpayalmath@gmail.com>
2026-01-21 19:55:54 +0530

    Add files via upload

---
+++ randomm3.py
@@ -0,0 +1,7 @@
+import math
+print("the value of 23.56 are:" + str(math.ceil(23.56)) + "," + str ( math.floor(23.56)))
+x = 10
+y = -15
+print("the value of x after copying the sighn of from y is:" + str math.copysign(x,y))
+print("absolute value of -96 and 56 are :") + str(math.copysign(x,y))
+print("the gcd of 24 and 56 is " + str(math.gcd(24,56)))
