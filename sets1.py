7ad2d04359ca25b3e2fe653539f72e93723b7608
shlok757 <singhpayalmath@gmail.com>
2026-02-11 19:30:51 +0530

    Add files via upload

---
+++ sets1.py
@@ -0,0 +1,10 @@
+my_set = {1,2,3}
+my_set = {1.0, "Hello",(1,2,3)}
+print(my_set)
+my_set = {1,2,3,4,3,2}
+print(my_set)
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("after removing first element from said set:")
+print(num_set,"/n")
