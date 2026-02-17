e497db822d30154576f0a8817ed528e425db5e5b
shlok757 <singhpayalmath@gmail.com>
2026-02-17 19:29:07 +0530

    Add files via upload

---
+++ advanced py2.py
@@ -0,0 +1,12 @@
+s1 = {2,3,1}
+s2 = {"b","a"<"c"}
+s3 = list(zip(s1,s2))
+print(s3,"\n")
+list = [10,20,30,40]
+list2 = [100,200,300,400]
+for x,y in zip(list,list2[::-1]):
+    print("x,y")
+    stocks = ['reliance','infoys','tcs']
+    prices = [2175,1127,2750]
+new_dict = (stocks: prices for stocks,prices in zip(stocks,prices))
+print('\n',format(new_dict))
