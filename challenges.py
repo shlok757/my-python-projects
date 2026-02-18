3a88f377a296f1206484728d9f0cd7d0e54f81de
shlok757 <singhpayalmath@gmail.com>
2026-02-18 19:50:43 +0530

    Add files via upload

---
+++ challenges.py
@@ -0,0 +1,28 @@
+theboard = {'7':'','8':'','9':'',
+            '4':'','5':'','6':'',
+            '1':'','2':'','3':''}
+board_keys = []
+for key in theboard:
+    board_keys.append(key)
+    def printboard(board):
+        print(board['7'] + 'I' board['8'] + 'I' board['9'])
+        print('-+-+-')
+        print(board['6'] + 'I' board['5'] + 'I' board['4'])
+         
+        print(board['3'] + 'I' board['2'] + 'I' board['1']):
+        def game():
+            turn = 'x'
+            count = 0
+            for i in range(10):
+                printboard(theboard)
+                print("its your turn"+ turn+ "MOVE WHICH PLACE")
+                move = input if theboard[move] == '':
+                 count + = 1
+else:
+   print("the place is already filled")
+   continue
+if count >= 5:
+   if theboard['7'] == theboard['8'] == theboard['9']!='':
+      printboard(theboard)
+      print('gaem over')
+      break
